0587f46e6e3ee9dc2a6309f44c85b2c986a39fc9
CodeBender <157215759+codeBender007@users.noreply.github.com>
2024-09-25 21:35:01 +0530

    Add Everything

---
+++ src/App.js
@@ -0,0 +1,57 @@
+<<<<<<< HEAD
+import logo from './logo.svg';
+import './App.css';
+=======
+import { Box, Container } from "@chakra-ui/react"
+import { Navigate, Route, Routes } from "react-router-dom"
+import UserPage from "./pages/UserPage"
+import PostPage from "./pages/PostPage"
+import Header from "./components/Header"
+import HomePage from "./pages/HomePage"
+import AuthPage from "./pages/AuthPage"
+import { useRecoilValue } from "recoil"
+import userAtom from "./atoms/userAtom"
+import LogoutButton from "./components/LogoutButton"
+import UpdateProfilePage from "./pages/UpdateProfilePage"
+import CreatePost from "./components/CreatePost"
+import ChatPage from "./pages/ChatPage"
+
+const API_BASE_URL = 'https://social-backend-6n19.onrender.com'
+>>>>>>> 45c2d93475e818fdb925297bdc528d5f0a109d42
+
+function App() {
+
+  const user = useRecoilValue(userAtom);
+  return (
+    <Box position={'relative'} w={'full'} >
+      <Container maxW="620px">
+        <Header />
+        <Routes>
+
+          <Route path='/' element={user ? <HomePage /> : <Navigate to="/auth" />} />
+          <Route path='/auth' element={!user ? <AuthPage /> : <Navigate to="/" />} />
+          <Route path='/update' element={user ? <UpdateProfilePage /> : <Navigate to="/auth" />} />
+
+          <Route path='/:username' element={user ?
+            (
+              <>
+                <UserPage />
+                <CreatePost />
+              </>
+            ) : (
+              <UserPage />
+            )
+          } />
+          <Route path="/:username/post/:pid" element={<PostPage />} />
+          <Route path="/chat" element={user ? <ChatPage /> : <Navigate to={'/auth'} />} />
+        </Routes>
+
+
+
+      </Container>
+    </Box>
+  )
+}
+
+export default App
+
